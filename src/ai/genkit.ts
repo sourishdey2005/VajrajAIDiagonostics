018609ee9d375ebc245975152bc27b7fe0bab161
Sourish Dey <134493350+sourishdey2005@users.noreply.github.com>
2025-10-15 07:09:41 +0530

    Update genkit.ts

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -2,6 +2,6 @@ import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/google-genai';
 
 export const ai = genkit({
-  plugins: [googleAI({apiKey: process.env.GEMINI_API_KEY})],
+  plugins: [googleAI({apiKey: process.env.AIzaSyCThb8aDkAmmrGphSwA7PzaL2QqQlvJXPQ})],
   model: 'googleai/gemini-2.5-flash',
 });
